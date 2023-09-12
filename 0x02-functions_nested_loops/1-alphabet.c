078070296dd7d38bb7423060a6ebf7c27c91206e
Daniel ATTITSOGBE <daniel.attitsogbe02@gmail.com>
2023-09-12 14:24:28 -0700

    Fix 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - function to print alphabet using _putchar
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@
 void print_alphabet(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
